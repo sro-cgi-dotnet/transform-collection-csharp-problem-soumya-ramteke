b56423bee22ec26c9d4fd72e4d5e8f83f3efc079
soumya-ramteke <soumya.r@cgi.com>
2018-09-12 16:15:56 +0530

    added 4assgnmnt .net

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -3,12 +3,19 @@ using System.Linq;
 using System.Collections.Generic;
 
 namespace Transform
-{
+{   
+    public delegate int ExpDelegate(int num);
     public static class Transform 
     {
-        public static void Map()
+        public static int[] Map(this int[] num_arr, ExpDelegate expDelegate)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+             // transform elements of the array one by one
+            int[] result = new int[num_arr.Length];
+            for(int i = 0; i < result.Length; i++){
+                result[i] = expDelegate(num_arr[i]);
+            }
+            return result;
         }
     }
 }
